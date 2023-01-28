185f6b917bb4ef4ebb073e8d4554de7c8a3e4812
0keanix <iodenisof@gmail.com>
2023-01-28 14:26:18 +0400

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = n * -1;
+    }
+    const arr = n.toString().split('');
+    let result = '';
+    for (let i = arr.length - 1; i >= 0; i--) {
+        result += arr[i];
+    }
+    return result;
 }
